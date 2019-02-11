1b17c05cae847070157b3fe598448e0ff928b2fd
MengfeiCao <657135629@qq.com>
2019-02-11 21:24:30 +0800

    banner图完成

--- dist/javascripts/index.js
+++ dist/javascripts/index.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var mySwiper=new Swiper(".swiper-container",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,3 +2,26 @@
 // .then((res)=>{
 //     console.log("完成代理");
 // });
+
+
+
+// banner图
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true, // 循环模式选项
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // 如果需要滚动条
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })     
